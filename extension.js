5df69c8ce95cf5222165f53ffe3d65dda108a7c9
dot-slash <slashdotdash@gmx.com>
2017-01-27 11:51:18 +0000

    Initial commit

---
+++ extension.js
@@ -0,0 +1,192 @@
+const St = imports.gi.St;
+const Main = imports.ui.main;
+
+const GLib = imports.gi.GLib;
+
+const Shell = imports.gi.Shell;
+const Util = imports.misc.util;
+const Lang = imports.lang;
+const PopupMenu = imports.ui.popupMenu;
+const PanelMenu = imports.ui.panelMenu;
+
+let TorproxyInstance, path, theme;
+
+function init(extensionMeta) {
+    // Set path for custom icon
+    path = extensionMeta.path;
+    theme = imports.gi.Gtk.IconTheme.get_default();
+    theme.append_search_path(path + "/icons");
+}
+
+function enable() {
+    TorproxyInstance = new TorproxyMenu(path);
+    Main.panel.addToStatusArea('Torproxy', TorproxyInstance);
+}
+
+function disable() {
+    TorproxyInstance.destroy();
+    TorproxyInstance = null;
+}
+
+const TorproxyMenu = new Lang.Class({
+    Name: 'TorProxy',
+    Extends: PanelMenu.Button,
+
+    _init: function(path) {
+        this.parent(null, 'TorProxy');
+
+        this.user = GLib.get_user_name();
+        this.filepath = path;
+        this.active = false;
+
+        this.icon_inactive = 120;
+        this.icon_active = 235;
+
+        this.addPanelButton();
+        this.createMenu();
+    },
+
+    addPanelButton: function() {
+        // Create panel button
+        this.button = new St.BoxLayout({ style_class: "main-button" });
+        this.icon = new St.Icon({
+            icon_name: 'tor-simple-symbolic',
+            style_class: 'system-status-icon'
+        });
+        this.icon.opacity = this.icon_inactive;
+        this.button.add_actor(this.icon);
+        this.actor.add_actor(this.button);
+
+        // Change icon opacity on hover
+        this.actor.connect('enter_event', Lang.bind(this, function() {
+            this.icon.opacity += 20;
+        }));
+        this.actor.connect('leave_event', Lang.bind(this, function() {
+            this.icon.opacity -= 20;
+        }));
+    },
+
+    createMenu: function() {
+        // Add menu items
+        this.torproxySwitch = new PopupMenu.PopupSwitchMenuItem("Torproxy");
+        //this.torproxySwitch._switch.reactive = false;
+        this.torproxySwitch.connect('toggled', Lang.bind(this, this.toggleSwitch));
+        this.menu.addMenuItem(this.torproxySwitch);
+
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem);
+
+	    this.menuChange = new PopupMenu.PopupMenuItem("Change TOR circuit");
+	    this.menuChange.actor.reactive = false;
+	    this.menuChange.connect('activate', Lang.bind(this, this._tpChange));
+	    this.menu.addMenuItem(this.menuChange);
+
+	    this.menuRestart = new PopupMenu.PopupMenuItem("Restart Torproxy");
+	    this.menuRestart.actor.reactive = false;
+	    this.menuRestart.connect('activate', Lang.bind(this, this._tpRestart));
+	    this.menu.addMenuItem(this.menuRestart);
+
+	    this.menuCheck = new PopupMenu.PopupMenuItem("Check TOR status");
+	    this.menuCheck.actor.reactive = false;
+	    this.menuCheck.connect('activate', Lang.bind(this, this._tpCheck));
+	    this.menu.addMenuItem(this.menuCheck);
+    },
+
+    toggleSwitch: function() {
+        if (this.active) {
+            this._tpStop();
+        } else {
+            this._tpStart();
+        }
+    },
+
+    setState: function(newState) {
+        switch (newState) {
+            case 'on':
+                this.icon.opacity = this.icon_active;
+                this.active = true;
+                this.torproxySwitch.setToggleState(true);
+                this.menuChange.actor.reactive = true;
+                this.menuRestart.actor.reactive = true;
+                this.menuCheck.actor.reactive = true;
+                break;
+            case 'off':
+                this.icon.opacity = this.icon_inactive;
+                this.active = false;
+                this.torproxySwitch.setToggleState(false);
+                this.menuChange.actor.reactive = false;
+                this.menuRestart.actor.reactive = false;
+                this.menuCheck.actor.reactive = false;
+                break;
+        }
+    },
+
+    _tpStart: function() {
+        this._spawn(['pkexec', this.filepath+'/torproxy.sh', 'start', this.user ], 'on');
+    },
+
+    _tpStop: function() {
+        this._spawn(['pkexec', this.filepath+'/torproxy.sh', 'stop', this.user ], 'off');
+    },
+
+    _tpChange: function() {
+        this._spawn(['pkexec', this.filepath+'/torproxy.sh', 'change', this.user ], null);
+    },
+
+    _tpRestart: function() {
+        this._spawn(['pkexec', this.filepath+'/torproxy.sh', 'restart', this.user ], null);
+    },
+
+    _tpCheck: function() {
+        this._spawn(['pkexec', this.filepath+'/torproxy.sh', 'status', this.user ], null);
+    },
+
+
+    _spawn: function(argv, newState) {
+        try {
+            // Spawn the process
+            let [exited, pid] = GLib.spawn_async(null,argv,null,
+                GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+                null
+            );
+
+            // Set callback for process completion
+            GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid,
+                Lang.bind(this, function(pid, exitStatus, requestObj) {
+
+                if(newState != null && exitStatus == "0") {
+                    // Change Torproxy state
+                    this.setState(newState);
+                    // Clean up when finished
+                    spawn_close_pid(pid);
+                } else if (newState != null && exitStatus != "0") {
+                    //Reset switch class
+                    switch (newState) {
+                        case 'on':
+                            this.torproxySwitch.setToggleState(false);
+                            break;
+                        case 'off':
+                            this.torproxySwitch.setToggleState(true);
+                            break;
+                    }
+                }
+            }));
+
+        } catch (err) {
+            if (err.matches(GLib.SpawnError, GLib.SpawnError.NOENT)) {
+                throw new GLib.SpawnError({ code: GLib.SpawnError.NOENT,
+                                            message: _("Command not found") });
+            } else if (err instanceof GLib.Error) {
+                message = err.message.replace(/.*\((.+)\)/, '$1');
+                throw new (err.constructor)({ code: err.code,
+                                              message: message });
+            } else {
+                throw err;
+            }
+        }
+    },
+
+    destroy: function() {
+        this.parent();
+    },
+
+});
---
+++ torproxy.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+
+# Torproxy is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# You can get a copy of the license at www.gnu.org/licenses
+#
+# Torproxy is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# For a copy of the GNU General Public License see:
+# <http://www.gnu.org/licenses/>.
+
+# Get DBUS address for notifications
+CURR_USER=$2
+PID=`pgrep --newest -u $CURR_USER gnome-shell`
+DBUS_SESSION_BUS_ADDRESS=`grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | sed -e 's/DBUS_SESSION_BUS_ADDRESS=//'`
+
+export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS
+
+# The UID Tor runs as. Check /usr/share/tor/tor-service-defaults-torrc "User" field for default.
+TOR_UID="debian-tor"
+
+# Tor's VirtualAddrNetworkIPv4
+VIRT_ADDR="10.192.0.0/10"
+
+# LAN destinations that shouldn't be routed through Tor
+NON_TOR="127.0.0.0/8 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16 127.0.0.0/9 127.128.0.0/10"
+
+# Other IANA reserved blocks (These are not processed by tor and dropped by default)
+RESV_IANA="0.0.0.0/8 100.64.0.0/10 169.254.0.0/16 192.0.0.0/24 192.0.2.0/24 192.88.99.0/24 198.18.0.0/15 198.51.100.0/24 203.0.113.0/24 224.0.0.0/3"
+
+# Tor's TransPort
+TRANS_PORT="9040"
+
+# DNS redirect port
+DNS_PORT="5353"
+
+
+function notify {
+	if [ -e /usr/bin/notify-send ]; then
+        local notice=$1
+        time=2500
+        path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+        icon=$path"/icons/icon180.png"
+        su "$CURR_USER" -c "notify-send Torproxy '$notice' -u low -t $time -i $icon"
+	fi
+}
+
+
+function init {
+	#notify "Killing dangerous processes and cleaning cache..."
+
+	echo -e "[Torproxy] Killing dangerous applications"
+	killall -q chrome dropbox iceweasel skype icedove thunderbird firefox chromium xchat transmission deluge pidgin pidgin.orig
+
+	#if ! [ hash bleachbit 2>/dev/null ]; then
+	#    echo -e -n "[Torproxy] Cleaning some dangerous cache elements"
+	#    bleachbit -c adobe_reader.cache chromium.cache chromium.current_session chromium.history elinks.history emesene.cache epiphany.cache firefox.url_history flash.cache flash.cookies google_chrome.cache google_chrome.history links2.history opera.cache opera.search_history opera.url_history &> /dev/null
+    #fi
+}
+
+function start {
+	check_root
+	disable_ufw
+
+	# Kill IPv6 services
+	echo -e "[Torproxy] Stopping IPv6 services..."
+	sed -i '/^.*\#Torproxy$/d' /etc/sysctl.conf
+	echo "net.ipv6.conf.all.disable_ipv6=1 #Torproxy" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.default.disable_ipv6=1 #Torproxy" >> /etc/sysctl.conf
+	sysctl -p > /dev/null
+
+	echo -e "[Torproxy] Starting anonymous mode:"
+	
+	if [ ! -e /var/run/tor/tor.pid ]; then
+	    notify "Starting TOR daemon..."
+
+		echo -e "[Torproxy] Tor is not running. Starting it for you" >&2
+		#service network-manager force-reload > /dev/null 2>&1
+        service resolvconf stop 2>/dev/null || true
+        service nscd stop 2>/dev/null || true
+        service dnsmasq stop 2>/dev/null || true
+        sleep 1
+		killall dnsmasq nscd resolvconf 2>/dev/null || true
+		sleep 2
+		service resolvconf start
+		sleep 1
+		#service tor start
+		tor --defaults-torrc /usr/share/tor/tor-service-defaults-torrc -f /home/user/.local/share/gnome-shell/extensions/torproxy@anon.null/torrc --RunAsDaemon 0
+		sleep 1
+	fi
+
+    # Save IP table rules
+	if ! [ -f /etc/network/iptables.rules ]; then
+		iptables-save > /etc/network/iptables.rules
+		echo -e "[Torproxy] Saved iptables rules"
+	fi
+
+	# Flush IP tables
+	iptables -F
+	iptables -t nat -F
+
+	# Set DNS resolver to use TOR
+	cp -vf /etc/resolv.conf /etc/resolv.conf.backup
+    echo -e 'nameserver 127.0.0.1' > /etc/resolv.conf
+
+    # Don't nat the TOR process itself
+    iptables -t nat -A OUTPUT -m owner --uid-owner $TOR_UID -j RETURN
+
+	# Nat DNS requests to TOR
+	iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports $DNS_PORT
+	iptables -t nat -A OUTPUT -p tcp --dport 53 -j REDIRECT --to-ports $DNS_PORT
+	iptables -t nat -A OUTPUT -p udp -m owner --uid-owner $TOR_UID -m udp --dport 53 -j REDIRECT --to-ports $DNS_PORT
+
+	# Nat .onion addresses
+	iptables -t nat -A OUTPUT -p tcp -d $VIRT_ADDR -j REDIRECT --to-ports $TRANS_PORT
+	iptables -t nat -A OUTPUT -p udp -d $VIRT_ADDR -j REDIRECT --to-ports $TRANS_PORT
+	
+	# Exclude local network addresses
+	for LAN in $NON_TOR; do
+		iptables -t nat -A OUTPUT -d $LAN -j RETURN
+		iptables -A OUTPUT -d $LAN -j ACCEPT
+	done
+
+	# Exclude IANA reserved blocks
+	for IANA in $RESV_IANA; do
+        iptables -t nat -A OUTPUT -d $IANA -j RETURN
+    done
+	
+	# Redirect all other output through TOR
+	iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $TRANS_PORT
+	iptables -t nat -A OUTPUT -p udp -j REDIRECT --to-ports $TRANS_PORT
+	iptables -t nat -A OUTPUT -p icmp -j REDIRECT --to-ports $TRANS_PORT
+	
+	# Accept already established connections
+	iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+	# Allow only TOR output
+	iptables -A OUTPUT -m owner --uid-owner $TOR_UID -j ACCEPT
+	iptables -A OUTPUT -j REJECT
+
+    # Filter input
+    iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
+    iptables -A INPUT -i lo -j ACCEPT
+	iptables -A INPUT -j DROP
+
+	# Filter output
+	iptables -A OUTPUT -m state --state INVALID -j DROP
+	iptables -A OUTPUT -d 127.0.0.1/32 -o lo -j ACCEPT
+
+	# Set default policies to DROP
+    iptables -P INPUT DROP
+    iptables -P FORWARD DROP
+    iptables -P OUTPUT DROP
+
+	echo -e "[Torproxy] All traffic is now redirected through Tor"
+
+	notify "Torproxy is up and running."
+}
+
+
+function stop {
+	check_root
+
+    notify "Shutting down Torproxy..."
+	echo -e "[Torproxy] Stopping Torproxy..."
+
+	iptables -F
+	iptables -t nat -F
+	echo -e "[Torproxy] Flushed iptables rules"
+	
+	if [ -f /etc/network/iptables.rules ]; then
+		iptables-restore < /etc/network/iptables.rules
+		rm /etc/network/iptables.rules
+		echo -e "[Torproxy] Iptables rules restored"
+	fi
+	
+	# Restore DNS settings
+	if [ -e /etc/resolv.conf.backup ]; then
+		rm /etc/resolv.conf
+		cp /etc/resolv.conf.backup /etc/resolv.conf
+	fi
+	
+	service tor stop
+
+	# Reenable IPv6 services
+	echo -e "[Torproxy] Reenabling IPv6 services"
+	sed -i '/^.*\#Torproxy$/d' /etc/sysctl.conf #delete lines containing #Torproxy in /etc/sysctl.conf
+	sysctl -p
+
+    # Reload services
+	service network-manager force-reload > /dev/null 2>&1
+	service nscd start > /dev/null 2>&1
+	service dnsmasq start > /dev/null 2>&1
+	
+	echo -e "[Torproxy] Anonymous mode stopped"
+    notify "Torproxy is now disabled."
+
+    enable_ufw
+}
+
+function change {
+	service tor reload
+	sleep 1
+	echo -e "[Torproxy] Tor daemon reloaded and forced to change nodes"
+
+	notify "TOR service reloaded."
+}
+
+function status {
+    status=`service tor status | grep Active: | sed -e 's/^.*\Active://'`
+	notify "Status: $status"
+}
+
+function check_root {
+    if [ $(id -u) -ne 0 ]; then
+		notify "Torproxy must be run as root."
+		echo -e "[Torproxy] Torproxy must be run as root" >&2
+		exit 1
+	fi
+}
+
+function disable_ufw () {
+	if hash ufw 2>/dev/null; then
+    	if ufw status | grep -q active$; then
+        	echo -e "[Torproxy] Disabling ufw firewall..."
+        	ufw disable
+        	sleep 6
+    	fi
+    fi
+}
+
+function enable_ufw () {
+	if hash ufw 2>/dev/null; then
+    	if ufw status | grep -q inactive$; then
+        	echo -e "[Torproxy] Re-enabling ufw firewall..."
+        	ufw enable
+        fi
+    fi
+}
+
+case "$1" in
+	start)
+		init
+		start
+	;;
+	stop)
+		init
+		stop
+	;;
+	change)
+		change
+	;;
+	status)
+		status
+	;;
+	restart)
+	    #notify "Restarting Torproxy..."
+		stop
+		sleep 2
+		start
+		#notify "Torproxy is up and running."
+	;;
+    *)
+        exit 1
+    ;;
+esac
+
+exit 0
